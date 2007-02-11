6dfb552f3d540c27ab6fc89283cb218ac7e9ae54
Martin Aspeli <optilude@gmail.com>
2007-02-11 03:40:14 +0000

    Updating setup.py
    svn path=/plone.app.iterate/trunk/; revision=12328

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(name='plone.app.iterate',
       version=version,
       description="check-out/check-in staging for Plone",
       long_description="""\
+plone.app.iterate is a check-out/check-in staging solution for Plone, 
+initially released as a Zope 2 product called "iterate". It uses CMFEditions,
+CMFDiffTool and other Plone 3 features.
 """,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
