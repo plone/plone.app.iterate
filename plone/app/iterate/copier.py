bf3bc5a567083914b634f9c4934a4da4a6e8f12f
Maurits van Rees <maurits@vanrees.org>
2011-02-11 00:50:01 +0000

    PPP (PeP8/Pyflakes)
    svn path=/plone.app.iterate/trunk/; revision=47420

--- plone/app/iterate/copier.py
+++ plone/app/iterate/copier.py
@@ -38,6 +38,7 @@ import interfaces
 from relation import WorkingCopyRelation
 from interfaces import CheckinException
 
+
 class ContentCopier( object ):
 
     interface.implements( interfaces.IObjectCopier )
@@ -48,25 +49,28 @@ class ContentCopier( object ):
 
     def copyTo( self, container ):
         wc = self._copyBaseline( container )
-        wc_ref = wc.addReference( self.context,
-                                  relationship = WorkingCopyRelation.relationship,
-                                  referenceClass = WorkingCopyRelation )
+        wc_ref = wc.addReference(
+            self.context,
+            relationship=WorkingCopyRelation.relationship,
+            referenceClass=WorkingCopyRelation)
         self._handleReferences( self.context, wc, "checkout", wc_ref )
         return wc, wc_ref
-    
+
     def merge( self ):
         baseline = self._getBaseline()
 
         # delete the working copy reference to the baseline
         wc_ref = self._deleteWorkingCopyRelation()
-        
-        # reassemble references on the new baseline         
+
+        # reassemble references on the new baseline
         self._handleReferences( baseline, self.context, "checkin", wc_ref )
 
-        # move the working copy to the baseline container, deleting the baseline 
+        # move the working copy to the baseline container, deleting
+        # the baseline
         new_baseline = self._replaceBaseline( baseline )
 
-        # patch the working copy with baseline info not preserved during checkout
+        # patch the working copy with baseline info not preserved
+        # during checkout
         self._reassembleWorkingCopy( new_baseline, baseline )
 
         return new_baseline
@@ -84,40 +88,43 @@ class ContentCopier( object ):
         baseline = refs[0]
         return baseline
 
-
     def _replaceBaseline( self, baseline ):
-        # move the working copy object to the baseline, returns the new baseline
+        # move the working copy object to the baseline, returns the
+        # new baseline
         baseline_id = baseline.getId()
-        
-        # delete the baseline from the folder to make room for the committed working copy
+
+        # delete the baseline from the folder to make room for the
+        # committed working copy
         baseline_container = aq_parent( aq_inner( baseline ) )
         baseline_container._delOb( baseline_id )
 
         # delete the working copy from the its container
-        wc_container =  aq_parent( aq_inner( self.context ) )
+        wc_container = aq_parent( aq_inner( self.context ) )
 
         # trick out the at machinery to not delete references
         self.context._v_cp_refs = 1
         self.context._v_is_cp = 0
-        
+
         wc_container.manage_delObjects( [self.context.getId()] )
-        
+
         # move the working copy back to the baseline container
         working_copy = aq_base( self.context )
         working_copy.setId( baseline_id )
         baseline_container._setOb( baseline_id, working_copy )
 
         new_baseline = baseline_container._getOb( baseline_id )
-        
+
         # reregister our references with the reference machinery after moving
-        Referenceable.manage_afterAdd( new_baseline, new_baseline, baseline_container)
-        
+        Referenceable.manage_afterAdd( new_baseline, new_baseline,
+                                       baseline_container)
+
         return new_baseline
 
     def _reassembleWorkingCopy( self, new_baseline, baseline ):
-        # reattach the source's workflow history, try avoid a dangling ref 
+        # reattach the source's workflow history, try avoid a dangling ref
         try:
-            new_baseline.workflow_history = PersistentMapping( baseline.workflow_history.items() )
+            new_baseline.workflow_history = PersistentMapping(
+                baseline.workflow_history.items())
         except AttributeError:
             # No workflow apparently.  Oh well.
             pass
@@ -143,8 +150,9 @@ class ContentCopier( object ):
         if original_back_refs != new_back_refs:
             self._removeDuplicateReferences(new_baseline, backrefs=True)
 
-        # reattach the source's history id, to get the previous version ancestry
-        histid_handler = getToolByName( self.context, 'portal_historyidhandler')
+        # reattach the source's history id, to get the previous
+        # version ancestry
+        histid_handler = getToolByName(self.context, 'portal_historyidhandler')
         huid = histid_handler.getUid( baseline )
         histid_handler.setUid( new_baseline, huid, check_uniqueness=False )
 
@@ -169,18 +177,18 @@ class ContentCopier( object ):
 
     def _deleteWorkingCopyRelation( self ):
         # delete the wc reference keeping a reference to it for its annotations
-        refs = self.context.getReferenceImpl( WorkingCopyRelation.relationship )
+        refs = self.context.getReferenceImpl(WorkingCopyRelation.relationship)
         wc_ref = refs[0]
         self.context.deleteReferences( WorkingCopyRelation.relationship )
         return wc_ref
 
     #################################
     ## Checkout Support Methods
-    
+
     def _copyBaseline( self, container ):
         # copy the context from source to the target container
         source_container = aq_parent( aq_inner( self.context ) )
-        clipboard = source_container.manage_copyObjects( [ self.context.getId() ] )
+        clipboard = source_container.manage_copyObjects([self.context.getId()])
         result = container.manage_pasteObjects( clipboard )
 
         # get a reference to the working copy
@@ -188,39 +196,36 @@ class ContentCopier( object ):
         target = container._getOb( target_id )
         return target
 
-    
     def _handleReferences( self, baseline, wc, mode, wc_ref ):
 
         annotations = IAnnotations( wc_ref )
-        
+
         baseline_adapter = interfaces.ICheckinCheckoutReference( baseline )
-        
+
         # handle forward references
         for relationship in baseline.getRelationships():
             # look for a named relation adapter first
-            adapter = component.queryAdapter( baseline,
-                                              interfaces.ICheckinCheckoutReference,
-                                              relationship )
-            
-            if adapter is None: # default
+            adapter = component.queryAdapter(
+                baseline, interfaces.ICheckinCheckoutReference, relationship )
+
+            if adapter is None:  # default
                 adapter = baseline_adapter
-                
+
             references = baseline.getReferenceImpl( relationship )
 
             mode_method = getattr( adapter, mode )
             mode_method( baseline, wc, references, annotations )
 
         mode = mode + "BackReferences"
-        
+
         # handle backward references
         for relationship in baseline.getBRelationships():
-            adapter = component.queryAdapter( baseline,
-                                              interfaces.ICheckinCheckoutReference,
-                                              relationship )
+            adapter = component.queryAdapter(
+                baseline, interfaces.ICheckinCheckoutReference, relationship )
             if adapter is None:
                 adapter = baseline_adapter
-                
+
             references = baseline.getBackReferenceImpl( relationship )
-            
+
             mode_method = getattr( adapter, mode )
             mode_method( baseline, wc, references, annotations )
