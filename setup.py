6a46cf4ba050945db9b98e508347fd41f156251c
Maurits van Rees <maurits@vanrees.org>
2016-04-26 14:07:08 +0200

    Preparing release 3.1.6
    [ci skip]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ LONG_DESCRIPTION = (
 
 setup(
     name='plone.app.iterate',
-    version='3.1.6.dev0',
+    version='3.1.6',
     description="check-out/check-in staging for Plone",
     long_description=LONG_DESCRIPTION,
     classifiers=[
