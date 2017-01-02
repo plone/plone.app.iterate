3da6e94d341620b630b87cc0a145e38e4f7c5f5a
esteele <eric@esteele.net>
2017-01-02 14:07:27 -0500

    Preparing release 3.2.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ LONG_DESCRIPTION = (
 
 setup(
     name='plone.app.iterate',
-    version='3.2.4.dev0',
+    version='3.2.4',
     description='check-out/check-in staging for Plone',
     long_description=LONG_DESCRIPTION,
     classifiers=[
