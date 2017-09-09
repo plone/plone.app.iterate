4fe186bc2fd6a77d3ff447090d042b58f158c7be
esteele <eric@esteele.net>
2017-09-08 22:23:20 -0400

    Back to development: 3.3.6
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ LONG_DESCRIPTION = (
 
 setup(
     name='plone.app.iterate',
-    version='3.3.5',
+    version='3.3.6.dev0',
     description='check-out/check-in staging for Plone',
     long_description=LONG_DESCRIPTION,
     classifiers=[
