e166c406d85abb7cbf6366f7edfc8c33ba6f75ea
David Glick <david@glicksoftware.com>
2018-05-08 10:38:24 -0400

    Run main tests against plone.app.contenttypes; skip AT tests if no ATContentTypes

--- plone/app/iterate/tests/test_doctests.py
+++ plone/app/iterate/tests/test_doctests.py
@@ -3,21 +3,28 @@ from plone.app.iterate.testing import PLONEAPPITERATE_FUNCTIONAL_TESTING
 from plone.app.iterate.testing import PLONEAPPITERATEDEX_FUNCTIONAL_TESTING
 from plone.testing import layered
 from unittest import TestSuite
-
 import doctest
+
+try:
+    import Products.ATContentTypes
+except ImportError:
+    HAS_AT = False
+else:
+    HAS_AT = True
 
 
 def test_suite():
     suite = TestSuite()
     OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
-    suite.addTest(layered(
-        doctest.DocFileSuite(
-            'browser.rst',
-            optionflags=OPTIONFLAGS,
-            package='plone.app.iterate.tests',
-        ),
-        layer=PLONEAPPITERATE_FUNCTIONAL_TESTING)
-    )
+    if HAS_AT:
+        suite.addTest(layered(
+            doctest.DocFileSuite(
+                'browser.rst',
+                optionflags=OPTIONFLAGS,
+                package='plone.app.iterate.tests',
+            ),
+            layer=PLONEAPPITERATE_FUNCTIONAL_TESTING)
+        )
     suite.addTest(layered(
         doctest.DocFileSuite(
             'dexterity.rst',
