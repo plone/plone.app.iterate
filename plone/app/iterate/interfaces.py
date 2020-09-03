434b862de2b339cb41c6b2e269d05d667169df62
Maurits van Rees <maurits@vanrees.org>
2020-09-03 22:07:53 +0200

    Fixed deprecation warning for IObjectEvent.

--- plone/app/iterate/interfaces.py
+++ plone/app/iterate/interfaces.py
@@ -28,7 +28,7 @@ from plone.app.iterate import PloneMessageFactory as _
 from plone.locking.interfaces import LockType
 from plone.locking.interfaces import MAX_TIMEOUT
 from zope import schema
-from zope.component.interfaces import IObjectEvent
+from zope.interface.interfaces import IObjectEvent
 from zope.interface import Attribute
 from zope.interface import Interface
 
