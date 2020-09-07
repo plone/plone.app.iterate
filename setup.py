d088a15f3820c860a39ca10539b87bcf3dc920e2
Maurits van Rees <maurits@vanrees.org>
2020-09-07 13:01:45 +0200

    Back to development: 3.3.16
    [ci skip]

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESCRIPTION = (
 
 setup(
     name='plone.app.iterate',
-    version='3.3.15',
+    version='3.3.16.dev0',
     description='check-out/check-in staging for Plone',
     long_description=LONG_DESCRIPTION,
     classifiers=[
