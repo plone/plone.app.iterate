0657581c9948814f449294202f98eca6972e7c3e
Maurits van Rees <maurits@vanrees.org>
2023-02-25 01:53:56 +0100

    Add python_requires.
    [ci skip]

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     namespace_packages=["plone", "plone.app"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     install_requires=[
         "Acquisition",
         "DateTime",
