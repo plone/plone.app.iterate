d8edd090e7c34546749cf6187a6ff8e8aade80e6
Maurits van Rees <maurits@vanrees.org>
2023-03-22 00:15:55 +0100

    Back to development: 5.0.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
 setup(
     name="plone.app.iterate",
-    version="5.0.1",
+    version="5.0.2.dev0",
     description="check-out/check-in staging for Plone",
     long_description=LONG_DESCRIPTION,
     classifiers=[
